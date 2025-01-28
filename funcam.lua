e6f6ce25a99a8026d649561efcde3f3108a5e978
nikiiii7 <alexandredasilvaaleman2010@gmail.com>
2025-01-28 00:16:42 +0000

    Delete funcam.lua

--- funcam.lua
+++
@@ -1,9 +0,0 @@
-local camera = game:GetService("Workspace").CurrentCamera
-camera.CameraType = Enum.CameraType.Scriptable
-
-local initialCameraPosition = camera.CFrame.Position
-local initialCameraRotation = camera.CFrame.Rotation
-
-game:GetService("RunService").RenderStepped:Connect(function()
-    camera.CFrame = CFrame.new(initialCameraPosition) * CFrame.Angles(0, humRoot.CFrame.Rotation.Y, 0)
-end)
